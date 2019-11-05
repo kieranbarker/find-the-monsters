648063474a6f18d19603285ff7d8cbe4d6bd1cbd
Kieran Barker <kieran11397@gmail.com>
2019-11-05 07:15:43 +0000

    Minify the CSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,25 @@
-var gulp   = require("gulp"),
-    terser = require("gulp-terser"),
-    rename = require("gulp-rename");
+// globals
+var gulp = require("gulp");
+var rename = require("gulp-rename");
 
-function minify() {
+// minify css
+var cleanCSS = require("gulp-clean-css");
+
+// minify js
+var terser = require("gulp-terser");
+
+function minifyCSS() {
+  return gulp.src("./assets/css/styles.css")
+    .pipe(rename("styles.min.css"))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest("./assets/css"));
+}
+
+function minifyJS() {
   return gulp.src("./assets/js/app.js")
     .pipe(rename("app.min.js"))
     .pipe(terser())
     .pipe(gulp.dest("./assets/js"));
 }
 
-exports.default = minify;
+exports.default = gulp.parallel(minifyCSS, minifyJS);
