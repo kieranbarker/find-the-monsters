c87e10c739f7e700c180718e05d36ede4153cc12
Kieran Barker <kieran11397@gmail.com>
2019-11-05 05:42:54 +0000

    Set up project structure

---
+++ assets/js/app.js
@@ -0,0 +1,22 @@
+;(function(d) {
+
+  /**
+   * Variables
+   */
+
+  var monsters = [
+    "monster1",
+    "monster2",
+    "monster3",
+    "monster4",
+    "monster5",
+    "monster6",
+    "monster7",
+    "monster8",
+    "monster9",
+    "monster10",
+    "monster11",
+    "sock"
+  ];
+
+})(document);
